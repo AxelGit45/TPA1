40e4afee0cfaeafda093b93e8c25b54a1ce8d0ed
Donk <mcondoripaye@frba.utn.edu.ar>
2026-07-09 19:51:44 +0000

    CRUD de rutas y Entregas.

--- DonaTrack/src/main/java/arg/com/utn/donatrack/logistica/Camion.java
+++ DonaTrack/src/main/java/arg/com/utn/donatrack/logistica/Camion.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 public class Camion {
 
-    private String patente;
+  private String patente;
     private Integer volumen;
     private Integer altura;
     private Integer capacidadDeCarga;
     private List<Bien> bienesTransportados;
+
+  public Camion() {
+  }
 
   public Camion(String patente, Integer volumen, Integer altura, Integer capacidadDeCarga) {
     this.patente = patente;
@@ -18,11 +21,15 @@ public class Camion {
     this.altura = altura;
     this.capacidadDeCarga = capacidadDeCarga;
   }
+  public String getPatente() {return patente;}
+  public Integer getVolumen() {return volumen;}
+  public Integer getAltura() {return altura;}
+  public Integer getCapacidadDeCarga() {return capacidadDeCarga;}
+  public List<Bien> getBienesTransportados() {return bienesTransportados;}
 
-  /*
-  {
-    camion,
-    la lista de destinos
-  }
-  */
+  public void setPatente(String patente) { this.patente = patente; }
+  public void setVolumen(Integer volumen) { this.volumen = volumen; }
+  public void setAltura(Integer altura) { this.altura = altura; }
+  public void setCapacidadDeCarga(Integer capacidadDeCarga) { this.capacidadDeCarga = capacidadDeCarga; }
+  public void setBienesTransportados(List<Bien> bienesTransportados) { this.bienesTransportados = bienesTransportados; }
 }
--- DonaTrack/src/main/java/arg/com/utn/donatrack/logistica/Ruta.java
+++ DonaTrack/src/main/java/arg/com/utn/donatrack/logistica/Ruta.java
@@ -6,11 +6,12 @@ import arg.com.utn.donatrack.estados.*;
 import java.util.List;
 
 public class Ruta {
-
+    private Long Id;
     private Camion camion;
     private List<Entrega> entregas;
 
-    public Ruta(Camion camion, List<Entrega> entregas) {
+    public Ruta(Long Id, Camion camion, List<Entrega> entregas) {
+      this.Id = Id;
       this.camion = camion;
       this.entregas = entregas;
     }
@@ -41,7 +42,11 @@ public class Ruta {
     entregas.forEach(entrega -> entrega.cambiarEstado(EstadoEntrega.ENTRASLADO));
   }
 
-  private Camion getCamion() {
+  public Camion getCamion() {
       return camion;
   }
+  public Long getId() { return Id; }
+  public List<Entrega> getEntregas() {return entregas;}
+  public void setCamion(Camion camion) { this.camion = camion; }
+  public void setEntregas(List<Entrega> entregas) { this.entregas = entregas; }
 }
